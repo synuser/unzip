9a57787406f1563125679a528612052450e3f975
Syn <38806740+synuser@users.noreply.github.com>
2021-01-11 11:37:29 +0000

    Update unzip.py

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -1,14 +1,25 @@
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
 
+import logging
 import os
 import shutil
 import subprocess
 import time
+from glob import glob
+
+import pyrogram
+# the logging things
+from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import humanbytes, progress_for_pyrogram
+from translation import Translation
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
@@ -16,25 +27,20 @@ else:
     from sample_config import Config
 
 # the Strings used for this "thing"
-from translation import Translation
 
-import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from helper_funcs.chat_base import TRChatBase
-from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["unzip"]))
 async def unzip(bot, update):
-    TRChatBase(update.from_user.id, update.text, "unzip")
-    if str(update.from_user.id) in Config.AUTH_USERS:
-        await bot.send_message(
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
             chat_id=update.chat.id,
-            text=Translation.NOT_AUTH_USER_TEXT,
-            reply_to_message_id=update.message_id
+            message_ids=update.message_id,
+            revoke=True
         )
         return
+    TRChatBase(update.from_user.id, update.text, "unzip")
     saved_file_path = Config.DOWNLOAD_LOCATION + \
         "/" + str(update.from_user.id) + ".unzip.zip"
     if os.path.exists(saved_file_path):
@@ -42,7 +48,7 @@ async def unzip(bot, update):
     reply_message = update.reply_to_message
     if ((reply_message is not None) and
         (reply_message.document is not None) and
-        (reply_message.document.file_name.endswith(Translation.UNZIP_SUPPORTED_EXTENSIONS))):
+            (reply_message.document.file_name.endswith(Translation.UNZIP_SUPPORTED_EXTENSIONS))):
         a = await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.DOWNLOAD_START,
@@ -54,7 +60,11 @@ async def unzip(bot, update):
                 message=reply_message,
                 file_name=saved_file_path,
                 progress=progress_for_pyrogram,
-                progress_args=(Translation.DOWNLOAD_START, a.message_id, update.chat.id, c_time)
+                progress_args=(
+                    Translation.DOWNLOAD_START,
+                    a,
+                    c_time
+                )
             )
         except (ValueError) as e:
             await bot.edit_message_text(
@@ -78,36 +88,41 @@ async def unzip(bot, update):
                 message_id=a.message_id
             )
             try:
-                command_to_exec = [
-                    "7z",
-                    "e",
-                    "-o" + extract_dir_path,
-                    saved_file_path
-                ]
-                # https://stackoverflow.com/a/39629367/4723940
-                logger.info(command_to_exec)
-                t_response = subprocess.check_output(
-                    command_to_exec, stderr=subprocess.STDOUT)
+                shutil.unpack_archive(saved_file_path, extract_dir_path)
+                # files = [f for f in glob(extract_dir_path,recursive=True) if os.path.isfile(f)]
+                # filename = set(list(files))
+
+                # command_to_exec = [
+                #     "unzip",
+                #     "-o",
+                #     saved_file_path,
+                #     # extract_dir_path,
+                #     "-d",
+                #     extract_dir_path
+                # ]
+                # # https://stackoverflow.com/a/39629367/4723940
+                # logger.info(command_to_exec)
+                # t_response = subprocess.check_output(
+                #     command_to_exec, stderr=subprocess.STDOUT)
                 # https://stackoverflow.com/a/26178369/4723940
             except:
                 try:
-                    #os.remove(saved_file_path)
+                    os.remove(saved_file_path)
                     shutil.rmtree(extract_dir_path)
                 except:
                     pass
-              #  await bot.edit_message_text(
-                 #   chat_id=update.chat.id,
-                 #   text=Translation.EXTRACT_ZIP_ERRS_OCCURED,
-                #    disable_web_page_preview=True,
-             #       parse_mode="html",
-           #         message_id=a.message_id
-       #         )
-      #      else:
+                await bot.edit_message_text(
+                    chat_id=update.chat.id,
+                    text=Translation.EXTRACT_ZIP_ERRS_OCCURED,
+                    disable_web_page_preview=True,
+                    parse_mode="html",
+                    message_id=a.message_id
+                )
+            else:
                 os.remove(saved_file_path)
                 inline_keyboard = []
                 zip_file_contents = os.listdir(extract_dir_path)
-                i = 0
-                for current_file in zip_file_contents:
+                for i, current_file in enumerate(zip_file_contents):
                     cb_string = "ZIP:{}:ZIP".format(str(i))
                     inline_keyboard.append([
                         pyrogram.InlineKeyboardButton(
@@ -115,7 +130,6 @@ async def unzip(bot, update):
                             callback_data=cb_string.encode("UTF-8")
                         )
                     ])
-                    i = i + 1
                 cb_string = "ZIP:{}:ZIP".format("ALL")
                 inline_keyboard.append([
                     pyrogram.InlineKeyboardButton(
